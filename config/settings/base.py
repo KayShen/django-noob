e8c42b88b13a260aae6722ab22d2bad1bb8fb388
Ke Shen <ke.shen@jet.com>
2017-08-11 11:49:15 -0400

    modified base

--- config/settings/base.py
+++ config/settings/base.py
@@ -10,12 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
-import os
 import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+ROOT_DIR = environ.Path(_file_)-3
+APPS_DIR = ROOT_DIR.path('project')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
