b7112433e2d651e6d2b27f68b3d99aa864024fee
KayShen <kshen4@me.com>
2017-08-14 13:25:29 -0400

    wip

--- project/api/urls.py
+++ project/api/urls.py
@@ -4,7 +4,9 @@ from project.api import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
